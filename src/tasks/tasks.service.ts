ae28b615b6744ac2b3ff56aa97a2b5dd1e9f5dce
sahilvyasrapidinnovation <sahilvyas@rapidiinovation.dev>
2022-12-23 13:43:29 +0530

    initial commit

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@nestjs/common';
+import { Task } from './task.model';
+import { TaskStatus } from './task.model';
+import { v4 } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/gets-tasks.dto';
+//import { title } from 'process';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  getTaskWithFilters(filterDto: GetTasksFilterDto): Task[] {
+    const { status, search } = filterDto;
+
+    let tasks = this.getAllTasks();
+
+    if (status) {
+      tasks = tasks.filter((task) => task.status === status);
+    }
+
+    if (search) {
+      tasks = tasks.filter((task)=> {
+        if (task.title.includes(search) || task.description.includes(search)) {
+          return true;
+        }
+      })
+    }
+    return tasks;
+  }
+
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+    const task: Task = {
+      id: v4(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+    return task;
+  }
+
+  deleteTask(id: string): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus) {
+    const task = this.getTaskById(id);
+    task.status = status;
+    return task;
+  }
+}
